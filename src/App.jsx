49f222d50e4923a59fcd466eaacf0c73b632d334
Willians-Hurtado <willianshurtado5@gmail.com>
2025-03-31 13:58:57 +0200

    Implemented form with state handling and POST request to submit blog post data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
 import React, { useState } from 'react';
 
 function App() {
+  const [formData, setFormData] = useState({
+    author: '',
+    title: '',
+    body: '',
+    state: ''
+  });
+
+  function handleFormData(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+
+    console.log(`[${e.target.name}]: ${e.target.value}`);
+
+  }
+
 
   return (
     <>
@@ -12,21 +29,32 @@ function App() {
 
           <form>
             <h3>Author</h3>
-            <input className="form-control mb-3" type="text" name="author" placeholder="Write the author's name" />
+            <input className="form-control mb-3" type="text" name="author" placeholder="Write the author's name" value={formData.author} onChange={handleFormData} />
 
             <h3>Title</h3>
-            <input className="form-control mb-3" type="text" name="title" placeholder="Write the title of the post" />
+            <input className="form-control mb-3" type="text" name="title" placeholder="Write the title of the post" value={formData.title} onChange={handleFormData} />
 
             <h3>Body</h3>
-            <textarea className="form-control mb-3" name="body" placeholder="Write the body of the post" />
+            <textarea className="form-control mb-3" name="body" placeholder="Write the body of the post" value={formData.body} onChange={handleFormData} />
 
             <h3>State</h3>
-            <select className="form-control mb-3" name="state">
+            <select className="form-control mb-3" name="state" value={formData.state} onChange={handleFormData}>
               <option value="public">Public</option>
               <option value="draft">Draft</option>
             </select>
 
-            <button type="submit" className="btn btn-primary" >
+            <button type="submit" className="btn btn-primary" onClick={(e) => {
+              e.preventDefault();
+              fetch('https://jsonplaceholder.typicode.com/posts', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+              })
+                .then(response => response.json())
+                .then(data => console.log(data))
+            }}>
               Submit
             </button>
 
